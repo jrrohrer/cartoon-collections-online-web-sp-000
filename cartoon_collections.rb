6af320faf9a6a364bc010ff31297610840a6a9d4
Jessica Rohrer <jessie.rohrer@gmail.com>
2020-09-04 15:37:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ def long_planeteer_calls(array)
   array.any? {|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.include?(cheese)
 end
